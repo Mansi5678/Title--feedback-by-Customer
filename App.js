2308bc5af88924ebb47e116457b5a5e9aa6718a9
Mansi Tiwari <157094775+Mansi5678@users.noreply.github.com>
2024-02-27 23:45:38 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import Testimonials from "./Testimonials";
+
+function App() {
+  return(
+    <>
+    <h1> FEEDBACK GIVEN BY CUSTOMER</h1>
+
+   <Testimonials />
+   
+   </>)
+}
+
+export default App;
